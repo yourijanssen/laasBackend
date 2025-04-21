5ca573f77663ab6966d54022c644da682e022a07
yourijanssen <64319462+yourijanssen@users.noreply.github.com>
2025-04-21 18:44:07 +0200

    added azure remote access

--- src/main/java/com/projectLaas/projectLaas/util/WebConfig.java
+++ src/main/java/com/projectLaas/projectLaas/util/WebConfig.java
@@ -13,6 +13,7 @@ public class WebConfig implements WebMvcConfigurer {
       public void addCorsMappings(CorsRegistry registry) {
             registry.addMapping("/**")
                         .allowedOrigins("http://localhost:4200")
+                        .allowedOrigins("https://victorious-tree-009534303.6.azurestaticapps.net/")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS");
       }
 
